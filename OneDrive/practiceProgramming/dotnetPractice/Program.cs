a280db3640709873aed890851f79f1a587565c37
Juan <juancalderonmicro@hotmail.com>
2025-09-04 13:40:15 -0500

    Third exercise solved

--- OneDrive/practiceProgramming/dotnetPractice/Program.cs
+++ OneDrive/practiceProgramming/dotnetPractice/Program.cs
@@ -50,7 +50,36 @@ namespace dotnetPractice
 
             Console.Write("The sum is " + sum);
 
-             */
+            
+
+
+            //ite a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. 
+            // For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+
+
+            -------2 EXERCISE 
+            int number = 0;
+            Console.Write("Enter a number: ");
+            bool userInput = int.TryParse(Console.ReadLine(), out number);
+            var result = 1;
+
+            if (userInput)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    result = result * i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Write an acual integer");
+            }
+            Console.WriteLine($"{number}! = {result}");
+        
+        */
+
         }
+        
     }
 }
